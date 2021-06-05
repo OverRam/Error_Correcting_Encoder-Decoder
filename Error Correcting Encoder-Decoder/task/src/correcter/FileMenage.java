e4d7335717878ea5033f3feb50ab9baf404f6ef8
Dominik <77927514+OverRam@users.noreply.github.com>
2021-06-06 00:35:15 +0200

    Addition of class FileMenage and methods readFileAsByteArr,  writeToFile, tryCreateFile.

---
+++ Error Correcting Encoder-Decoder/task/src/correcter/FileMenage.java
@@ -0,0 +1,40 @@
+package correcter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+class FileMenage {
+    static byte[] readFileAsByteArr(String path) {
+        try {
+            return Files.readAllBytes(Path.of(path));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    static void writeToFile(byte[] arr, String fileName) {
+        tryCreateFile(fileName);
+        try (OutputStream printStream = new FileOutputStream(fileName)) {
+            printStream.write(arr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void tryCreateFile(String patch) {
+        File file = new File(patch);
+        if (!file.exists()) {
+            try {
+                Files.createFile(Path.of(patch));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
