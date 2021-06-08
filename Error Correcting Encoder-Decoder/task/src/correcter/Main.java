887bd4cc9c190f83daccc89b95166117c2259532
Dominik <77927514+OverRam@users.noreply.github.com>
2021-06-08 23:15:16 +0200

    edit parity, now returns a checksum in the last 2 bits

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -85,7 +85,16 @@ public class Main {
     }
 
     private static String parity(String exp) {
-        return exp.replaceAll("\\.", "0");
+        StringBuilder sb = new StringBuilder();
+        String[] arrStr = exp.split(" ");
+        int count;
+
+        for (String s : arrStr) {
+            count = 0;
+            count += s.split("11").length;
+            sb.append(s.replaceAll("\\.", count % 2 == 0 ? "1" : "0")).append(" ");
+        }
+        return sb.toString();
     }
 
     private static void modeDecode() {
